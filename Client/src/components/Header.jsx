0e9fa37cc45320f099c39a623e10ebd327a77a63
Samantha Allen <samanthaamiraallen@gmail.com>
2024-05-19 12:23:45 -0300

    added Start of Header

---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Button color="inherit">Subscribe</Button>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Miss Mental Matters
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
