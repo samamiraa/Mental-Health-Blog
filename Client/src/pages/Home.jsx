dacbd11508df2ad3c459ea7869f28ff9e0745df9
Samantha Allen <samanthaamiraallen@gmail.com>
2024-05-24 10:42:51 -0300

    added more text to home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
 import Grid from '@mui/material/Grid';
+import { Paper } from '@mui/material';
 
 export default function Home() {
     return (
@@ -15,10 +16,25 @@ export default function Home() {
                     />
                 </Grid>
                 <Grid item xs={12} sm={8}>
-                    <Typography variant="h6" textAlign="center" gutterBottom sx={{ backgroundColor: '#f3eee8', color: "#eb9aa3", padding: '1rem', margin: '3rem', borderRadius: '20px', border: '2px solid black' }}>
-                        {"Don't believe everything you think"}
-                        <LocalFloristIcon sx={{ color: '#fac04b' }} />
-                    </Typography>
+                    <Paper sx={{ backgroundColor: '#f3eee8',  border: '2px solid black', margin: '2rem' }}>
+                        <Typography variant="h5" textAlign="center" gutterBottom sx={{  color: "#eb9aa3", padding: '1rem' }}>
+                            {"Don't believe everything you think"}
+                            <LocalFloristIcon sx={{ color: '#fac04b' }} />
+                        </Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
+            <Grid container alignItems="center" justifyContent="center">
+                <Grid item justifyContent='center'>
+                    <Paper sx={{ padding: '2rem', margin: '2rem', border: ' 2px solid black', backgroundColor: '#f3eee8' }}>
+                        <Typography variant='subtitle1' textAlign="center" sx={{ color: "#eb9aa3" }} gutterBottom>
+                            Be the change that you wish to see in the world. - Ghandhi
+                        </Typography>
+
+                        <Typography variant='body' align='justify'>
+                           
+                        </Typography>
+                    </Paper>
                 </Grid>
             </Grid>
         </div>
