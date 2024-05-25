34d8afd7123bde6d63663f1bbc6ae7900e99d0c7
Samantha Allen <samanthaamiraallen@gmail.com>
2024-05-25 12:21:39 -0300

    finished home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
 import Grid from '@mui/material/Grid';
-import { Paper } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 
 export default function Home() {
     return (
@@ -16,8 +16,8 @@ export default function Home() {
                     />
                 </Grid>
                 <Grid item xs={12} sm={8}>
-                    <Paper sx={{ backgroundColor: '#f3eee8',  border: '2px solid black', margin: '2rem' }}>
-                        <Typography variant="h5" textAlign="center" gutterBottom sx={{  color: "#eb9aa3", padding: '1rem' }}>
+                    <Paper sx={{ backgroundColor: '#f3eee8', border: '2px solid black', margin: '2rem' }}>
+                        <Typography variant="h5" textAlign="center" gutterBottom sx={{ color: "#eb9aa3", padding: '1rem' }}>
                             {"Don't believe everything you think"}
                             <LocalFloristIcon sx={{ color: '#fac04b' }} />
                         </Typography>
@@ -28,12 +28,18 @@ export default function Home() {
                 <Grid item justifyContent='center'>
                     <Paper sx={{ padding: '2rem', margin: '2rem', border: ' 2px solid black', backgroundColor: '#f3eee8' }}>
                         <Typography variant='subtitle1' textAlign="center" sx={{ color: "#eb9aa3" }} gutterBottom>
-                            Be the change that you wish to see in the world. - Ghandhi
+                            {"You don't have to control your thoughts. You just have to stop letting them control you. - Dan Millman"}
                         </Typography>
 
                         <Typography variant='body' align='justify'>
-                           
+                            {"Hey there, I just wanted to take a moment to remind you how incredibly brave you are. Every day, you navigate through a world that doesn't always understand the battles you face within your own mind. But despite the challenges, you keep pushing forward, and that takes an immense amount of strength. Your mental health journey is uniquely yours, and it's okay to take it one step at a time. Some days might feel like a struggle, but please remember that asking for help is a sign of courage, not weakness. You are not alone on this journey; there are people who care about you and want to support you in any way they can. It's important to prioritize self-care and kindness towards yourself. Whether it's taking a moment to breathe deeply, practicing mindfulness, or reaching out to a friend or professional for support, know that every small step you take towards your well-being is a victory. You are resilient, you are worthy, and you are capable of overcoming any obstacle that comes your way. Your mental health matters, and so do you. Take care, and remember to be gentle with yourself."}
                         </Typography>
+
+                        <div style={{ textAlign: 'center' }}>
+                            <Button sx={{ color: '#eb9aa3'  }}>
+                                Resources
+                            </Button>
+                        </div>
                     </Paper>
                 </Grid>
             </Grid>
