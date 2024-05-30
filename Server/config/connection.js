29a832de343d90b3f1bb59dd78ff11bbcc816025
Samantha Allen <samanthaamiraallen@gmail.com>
2024-05-30 09:52:03 -0300

    added in connected.js boiler plate

---
+++ Server/config/connection.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mentalhealthblog', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to mongoDB:', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
